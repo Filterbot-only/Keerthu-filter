8da53b6a710a623308040d350f76765ed9944c61
Filterbot-only <147235633+Filterbot-only@users.noreply.github.com>
2023-10-22 17:11:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -63,14 +63,14 @@ LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies 
 LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
-HOW_TO_DOWNLOAD =  environ.get('HOW_TO_DOWNLOAD', '')
+HOW_TO_DOWNLOAD =  environ.get('HOW_TO_DOWNLOAD', '600')
 
 AUTO_DELETE_SECONDS = int(environ.get('AUTO_DELETE_SECONDS', '300'))
 
 FILE_REQ_CHANNEL = int(environ.get('FILE_REQ_CHANNEL', LOG_CHANNEL))
 
-SHORTNER_SITE =  environ.get('SHORTNER_SITE', '') #Put Only Shortner Site domain don't put like this https://tnlink.in/
+SHORTNER_SITE =  environ.get('SHORTNER_SITE', 'tnshort.net') #Put Only Shortner Site domain don't put like this https://tnlink.in/
 
-SHORTNER_API =  environ.get('SHORTNER_API', '')
+SHORTNER_API =  environ.get('SHORTNER_API', '02c93c55c4567035a37ffc32731d8f0e1c530f98')
 
 AUTO_DELETE =  environ.get('AUTO_DELETE', 'True')
